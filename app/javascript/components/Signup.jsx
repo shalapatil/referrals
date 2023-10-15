76035de78543e95fa3b985b62ac9c1369f715187
Shalaka Patil <shalu214patil@gmail.com>
2023-10-15 12:18:57 +0530

    Show error on signup

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -13,6 +13,7 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import authenticationApi from "../apis/authentication";
+import Alert from "@material-ui/lab/Alert";
 
 function Copyright(props) {
   return (
@@ -37,94 +38,144 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function SignUp() {
+  const [alert, setAlert] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     let email = data.get("email");
     let password = data.get("password");
-    let res = await authenticationApi.signup({ email, password });
-
-    // console.log(res.data)
-    // localStorage.setItem("user", res.data.user.id);
+    let repassword = data.get("repassword");
+    if (!email || !password || !repassword) {
+      setAlert("Please enter email and password!");
+      return;
+    }
+    if (password === repassword) {
+      try {
+        let res = await authenticationApi.signup({ email, password });
+        console.log("res", res);
+        if (res.status === 200) {
+          setAlert("");
+          setSuccess(true);
+        }
+      } catch (error) {
+        setAlert("Please use valid email and password of at least length 6");
+      }
+    } else {
+      setAlert("Passwords doesn't match");
+    }
   };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
+  const renderSignUp = () => {
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          {alert && <Alert severity="error">{alert}</Alert>}
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="given-name"
-                  name="email"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="repassword"
-                  label="Confirm Password"
-                  name="repassword"
-                  autoComplete="repassword"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mx: 20 }}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
             >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="email"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="repassword"
+                    type="password"
+                    label="Confirm Password"
+                    name="repassword"
+                    autoComplete="repassword"
+                  />
+                </Grid>
               </Grid>
-            </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mx: 20 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
+    );
+  };
+
+  const renderSuccessSignup = () => {
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          {alert && <Alert severity="error">{alert}</Alert>}
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 100,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Successfully signed up!
+            </Typography>
+            <Link href="/" variant="body2">
+              Please login!
+            </Link>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
+  };
+
+  return success ? renderSuccessSignup() : renderSignUp();
 }
